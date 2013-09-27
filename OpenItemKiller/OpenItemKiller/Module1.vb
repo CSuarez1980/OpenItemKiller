c4da9c67c4113d2a6a51ff119a71d00aa2e63f9d
Cristian Suárez <gecocr@hotmail.com>
2013-09-27 07:50:35 -0600

    Initial commit

---
+++ OpenItemKiller/OpenItemKiller/Module1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Security
+
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("Killing Open Item Process...")
+        Dim cn As New OAConnection.Connection
+
+        Dim Process() As System.Diagnostics.Process = System.Diagnostics.Process.GetProcessesByName("OpenItems")
+        On Error Resume Next
+
+        For Each P In Process
+            P.Kill()
+        Next
+
+        Console.WriteLine("Restarting Open Item Process...")
+        System.Diagnostics.Process.Start(My.Application.Info.DirectoryPath & "\OpenItems.exe")
+    End Sub
+
+End Module
